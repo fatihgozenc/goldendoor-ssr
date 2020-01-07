6e3dd18ae78baa2db97e238b72d921bb01c54280
Fatih Gozenc <fatihgozenc@gmail.com>
2020-01-07 14:46:21 +0100

    desktop contact finalized

--- src/client/pages/ContactPage.js
+++ src/client/pages/ContactPage.js
@@ -15,20 +15,48 @@ const ContactPage = () => {
   const [endTime, setEndTime] = useState(new Date());
 
   const formSteps = React.useRef();
+  const stepSlider = React.useRef();
+  const dateRange = React.useRef();
+  const dates = React.useRef();
 
   const changeStep = (e) => {
-    if (e.currentTarget.classList.contains('goBack')) {
+    const ellipses = formSteps.current.parentElement.parentElement.firstElementChild.children;
+    if (e.currentTarget.classList.contains('goFirst')) {
       formSteps.current.classList.remove('atSecondStep')
+      ellipses[1].classList.remove('activeStep')
+      ellipses[0].classList.add('activeStep')
     } else if (e.currentTarget.classList.contains('goNext')) {
       formSteps.current.classList.remove('atSecondStep')
       formSteps.current.classList.add('atThirdStep')
+      ellipses[1].classList.remove('activeStep')
+      ellipses[2].classList.add('activeStep')
     } else if (e.currentTarget.classList.contains('goSecond')) {
       formSteps.current.classList.remove('atThirdStep')
       formSteps.current.classList.add('atSecondStep')
+      ellipses[2].classList.remove('activeStep')
+      ellipses[1].classList.add('activeStep')
     } else {
       formSteps.current.classList.add('atSecondStep')
+      ellipses[0].classList.remove('activeStep')
+      ellipses[1].classList.add('activeStep')
     }
   }
+
+  const hideDates = (e) => {
+    const answerYes = e.currentTarget.parentElement.parentElement.children[1].firstElementChild;
+    if (answerYes.checked ) {
+      dates.current.classList.remove('hidden')
+      dateRange.current.classList.add('hidden')
+    } else {
+      dates.current.classList.add('hidden')
+      dateRange.current.classList.remove('hidden')
+    }
+  }
+
+  React.useEffect(() => {
+    stepSlider.current.firstElementChild.classList.add('activeStep')
+    // dates.current.classList.add('hideDates')
+  }, []);
 
   console.log(data);
 
@@ -40,10 +68,13 @@ const ContactPage = () => {
       </div>
       <div className="contact">
 
-        <div className="contact__stepslider">
+        <div ref={stepSlider} className="contact__stepslider">
         {
           steps.map((item, key) => (
-          <div key={key} className="contact__stepslider--ellipse activeStep"><span>{key + 1}</span></div>
+          <div key={key}
+            className="contact__stepslider--ellipse">
+            <span>{key + 1}</span>
+          </div>
           ))
         }
         </div>
@@ -53,6 +84,8 @@ const ContactPage = () => {
         <form className="contact__steps" ref={formSteps}>
 
           <div className="contact__step">
+
+          <div className="contact__step--flexWrapper">
 
             <div className="contact__stepblock">
               <label htmlFor="event_type" className="label__select">{steps[0].kontakt_frage_1.frage}</label>
@@ -76,23 +109,29 @@ const ContactPage = () => {
               </select>
             </div>
 
+          </div>
+
+          <div className="contact__step--flexWrapper-xl">
+
             <div className="contact__stepblock">
               <label htmlFor="event_date_answer"
                 className="contact__stepblock--inner">{steps[0].kontakt_frage_3.frage}</label>
               {steps[0].kontakt_frage_3.optionen.map((item, key) => (
-                <p className="contact__stepblock--inner" key={key}>
-                  <input className="input__radio" type="radio" name="event_date_answer" value={item.option}/>
-                  <span className="label__radio">{item.option}</span>
-                </p>
+                  <p className="contact__stepblock--inner" key={key}>
+                    <input onClick={hideDates} className="input__radio" type="radio" name="event_date_answer" value={item.option}/>
+                    <span className="label__radio">{item.option}</span>
+                  </p>
               ))}
             </div>
 
-            <div className="contact__stepblock">
-              <label htmlFor="date_range">{steps[0].kontakt_frage_5}</label>
-              <input className="input__text" type="text" id="event_date_answer"/>
+            <div className="contact__stepblock" ref={dateRange}>
+              <label className="label__text" htmlFor="date_range">{steps[0].kontakt_frage_5}</label>
+              <input  className="input__text" required type="text" id="event_date_answer"/>
             </div>
 
-            <div className="contact__stepblock contact__stepblock--datewrapper">
+            <div
+              ref={dates}
+              className="contact__stepblock contact__stepblock--datewrapper hidden">
 
               <div className="contact__stepblock--date">
                 <label htmlFor="date_of_event">{steps[0].kontakt_frage_6.datum}</label>
@@ -128,39 +167,50 @@ const ContactPage = () => {
                 />
               </div>
 
-              <a
-                className="golden__button golden__button--center"
-                onClick={changeStep} >
-                {data.fields.buttonfelder.prufen}
-              </a>
-
             </div>
+
+          </div>
+
+
+
+            <a
+              href="#"
+              className="golden__button golden__button--center"
+              onClick={changeStep} >
+              {data.fields.buttonfelder.prufen}
+            </a>
 
           </div>
 
           <div className="contact__step">
 
+            <h3>{steps[1].feld_1.titel}</h3>
+
             <div className="contact__step--wrapper">
 
-              <h3>{steps[1].feld_1.titel}</h3>
+              <div className="contact__step--flexWrapper">
 
-              <div className="contact__stepblock">
-                <label htmlFor="people_number">{steps[1].feld_1.frage_1}</label>
-                <input type="text" className="input__text" name="people_number"/>
-              </div>
+                <div className="contact__stepblock">
+                  <label className="label__text" htmlFor="people_number">{steps[1].feld_1.frage_1}</label>
+                  <input type="text"  required className="input__text" name="people_number"/>
+                </div>
 
-              <div className="contact__stepblock">
-                <label htmlFor="event_budget"
-                className="contact__stepblock--inner">{steps[1].feld_1.frage_2.fragefeld}</label>
-                {Object.values(steps[1].feld_1.frage_2.optionen).map((item, key) => (
-                  <p className="contact__stepblock--inner" key={key}>
-                    <input type="radio" className="input__radio" name="event_budget" value={item}/>
-                    <span className="label__radio">{item}</span>
-                  </p>
-                ))}
+                <div className="contact__stepblock">
+                  <label htmlFor="event_budget"
+                  className="contact__stepblock--inner">{steps[1].feld_1.frage_2.fragefeld}</label>
+                  {Object.values(steps[1].feld_1.frage_2.optionen).map((item, key) => (
+                    <p className="contact__stepblock--inner" key={key}>
+                      <input type="radio" className="input__radio" name="event_budget" value={item}/>
+                      <span className="label__radio">{item}</span>
+                    </p>
+                  ))}
+                </div>
+
               </div>
 
               <h3>{steps[1].feld_2.titel}</h3>
+
+              <div className="contact__step--flexWrapper-xl">
 
               {(Object.values(steps[1].feld_2).slice(1)).map((item, key) => (
 
@@ -176,13 +226,17 @@ const ContactPage = () => {
                 </div>
               ))}
 
+</div>
+
             </div>
 
             <div className="contact__buttongroup">
               <a
+                href="#"
                 onClick={changeStep}
-                className="golden__button goBack">{data.fields.buttonfelder.zuruck}</a>
+                className="golden__button goFirst">{data.fields.buttonfelder.zuruck}</a>
               <a
+                href="#"
                 onClick={changeStep}
                 className="golden__button goNext">{data.fields.buttonfelder.weiter}</a>
             </div>
@@ -193,27 +247,52 @@ const ContactPage = () => {
 
             <h3>{steps[2].titel}</h3>
 
-            <div className="contact__stepblock">
-              <label htmlFor="event_location" className="label__select">{steps[2].anredefeld.frage}</label>
-              <select name="event_location">
-                {steps[2].anredefeld.sex.map((item, key) => (
-                  <option key={key} value={item.option}>{item.option}</option>
-                ))}
-              </select>
-            </div>
+            <div className="contact__step--flexWrapper">
 
-            {Object.values(steps[2].andere_fragen).map((item, key) => (
-
-              <div key={key} className="contact__stepblock">
-                <label htmlFor={`contactdata_${item}`}>{item}</label>
-                <input className="input__text" type="text" id={`contactdata_${item}`} name={`contactdata_${item}`}/>
+              <div className="contact__stepblock">
+                <label htmlFor="event_location" className="label__select">{steps[2].anredefeld.frage}</label>
+                <select name="event_location">
+                  {steps[2].anredefeld.sex.map((item, key) => (
+                    <option key={key} value={item.option}>{item.option}</option>
+                  ))}
+                </select>
               </div>
 
-            ))}
+              {Object.values(steps[2].andere_fragen).map((item, key) => (
+
+                <div key={key} className="contact__stepblock">
+                  <label className="label__text" htmlFor={`contactdata_${item}`}>{item}</label>
+                  { key === 0 | 4
+                    ? (
+                    <input
+                      className="input__text"
+                      type="text" id={`contactdata_${item.toLowerCase()}`}
+                      name={`contactdata_${item.toLowerCase()}`}/>
+                    ) : (
+                    <input
+                      required
+                      className="input__text"
+                      type="text" id={`contactdata_${item.toLowerCase()}`}
+                      name={`contactdata_${item.toLowerCase()}`}/>
+                    )
+                  }
+                </div>
+
+              ))}
+
+              <div className="contact__stepblock">
+                <label htmlFor="event__nachricht">{steps[0].kontakt_frage_4}</label>
+                <textarea
+                  name="event__nachricht"
+                  id="event__nachricht" cols="30" rows="5"/>
+              </div>
+
+            </div>
 
             <div className="contact__buttongroup">
 
               <a
+                href="#"
                 onClick={changeStep}
                 className="golden__button goSecond">{data.fields.buttonfelder.zuruck}</a>
 
